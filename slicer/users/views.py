1c683807ec9db68ce025c24ddfc18cc01e4e5711
Lyssa1101 <lyssa1101@gmail.com>
2024-11-21 14:30:15 +0100

    add users

--- slicer/users/views.py
+++ slicer/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after registration
+            return redirect('home')  # Redirect to home or any other page
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
